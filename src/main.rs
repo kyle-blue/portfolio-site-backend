48ae5ee69307e0c6269dbb699a33d0f407292ca1
Kyle Doidge <kyle.blue.nuttall@gmail.com>
2025-02-13 23:27:43 +0000

    Add rust boilerplate
    Additionally add helper outline comments

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+/**
+ * Make a server with:
+ *    port, handlers (which allow middleware), overall middleware,
+ *
+ * Define routes
+ *
+ * Defined methods
+ *
+ * Defined error codes
+ *
+ * Main server run function
+ *
+ * Async thread handle function
+ */
+
+fn root_handler() {}
+
+#[tokio::main]
+async fn main() {
+
+    // let server = Server {
+    //     port: 8080,
+    //     middleware: [],
+    // }
+
+    // server.route(HTTPMethod::Get, "/", root_handler);
+
+    // server.start().await?;
+}
