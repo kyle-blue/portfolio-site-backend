a26db1ba7edf8d258de10a8d41416686f84b2768
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2025-02-17 11:59:28 +0000

    Fix route pattern matching
    Routes now use a glob-like syntax to match. Using * will match up until
    the next /. Using :x will create a url param called x and save that
    param in the params hashmap. Using ** will match anything after that
    point.
    
    There is also a matching order:
    1. Longest match without a pattern such as *, **, or :x
    2. Longest match with most params like :x
    3. (if 0 params) Longest match with *
    4. Longest match with **

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 mod http;
 
+use http::core::*;
 use std::error::Error;
 
-use http::core::*;
-
-route!(root_handler, {
+route!(root_handler, |request: Request| {
     let mut response = Response::new();
     let body = String::from("Hello bossman");
+    response.set_body(body.into_bytes());
+    Some(response)
+});
+
+route!(test_handler, |request: Request| {
+    let mut response = Response::new();
+    let id = request.params.get("id").unwrap();
+    let body = format!("{{\"id\": \"{id}\"}}");
+    response.set_body(body.into_bytes());
+    Some(response)
+});
+
+route!(greg, |request: Request| {
+    let mut response = Response::new();
+    let body = format!("{}", request.path);
     response.set_body(body.into_bytes());
     Some(response)
 });
@@ -15,6 +29,8 @@ route!(root_handler, {
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut server = Server::new(8080);
     server.route(HttpMethod::GET, "/", root_handler);
+    server.route(HttpMethod::GET, "/:id", test_handler);
+    server.route(HttpMethod::GET, "/**", greg);
     server.start().await?;
     Ok(())
 }
